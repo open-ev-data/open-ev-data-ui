348ce9a14a743b1ab26fa49c47134bc0b631d93f
Wellington Ramos <wellingtonjsramos@hotmail.com>
2026-02-17 11:17:23 -0300

    feat: implement console introduction loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { Providers } from "./app/providers";
-import { router } from "./app/router";
-import "@/shared/styles/global.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { Providers } from './app/providers';
+import { router } from './app/router';
+import logo from '@/assets/text/open-ev-data.txt?raw';
+import introduction from '@/assets/text/introduction.txt?raw';
+import '@/shared/styles/global.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+console.log(`${logo}\n${introduction}`);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Providers>
       <RouterProvider router={router} />
     </Providers>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
